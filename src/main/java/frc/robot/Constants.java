683768981511b0d7290d1bf893ae471f640ec158
Krish Gupta <krishgupta2025@gmail.com>
2023-08-12 20:39:05 -0400

    setup

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -14,54 +16,14 @@ package frc.robot;
  */
 public final class Constants
 {
-  //PID Constants
-    public static final double kDt = 0.02;
-    //Drivetrain
-      public static final double kP = 0.0046211;
-      public static final double kD = 0.0033563;
-      public static final double kF = 0.02380952380952381;
-      public static final double kWheelDiameter = 0.1524;
-      public static final double kTurnP = 0.0024972;//0.000005; //max output/max error
-      public static final double kTurnD = 0;//0.0033563;//
-      public static final double kAutoP = 3.6/13;
-    //Arm
-      public static final double kArmP = 0.05; //0.052272
-      public static final double kArmI = 0;
-      public static final double kArmD = 0.02;
-      public static final double kArmFF = 0;
-      public static final double kArmIZone = 0;
-      //PID Max Constraints
-        public static final double kMaxVelocityDegPerSecond = 80;
-        public static final double kMaxAccelerationDegPerSecSquared = 100;
-        public static final double mGoal = 0;
-        public static final double msetpoint = 0;
+  public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
+  public static final double kDriveMotorGearRatio = 1/1;
+  public static final double kTurinigMotorGearRatio = 1/1;
+  public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio*Math.PI*kWheelDiameterMeters;
+  public static final double kTurningEncoderRot2Rad = kTurinigMotorGearRatio*2*Math.PI;
+  public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+  public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
+  public static final double kTurnP = 0.005;
 
-  //DriveTain.java
-    //CANIDS
-      public static final int LeftMasterCANID = 11;
-      public static final int LeftSlaveCANID = 13;
-      public static final int RightMasterCANID = 12;
-      public static final int RightSlaveCANID = 14;
-      public static final int mainControllerPort = 0; 
-    //Others
-      public static double turnAngle = 0.0;
-  //Arm.java 
-    //CANDIS
-    public static final int LeftArmCANID = 31;
-    public static final int RightArmCANID = 32;
-    public static double presetPosition = 0.0;
-
-    //Constants for feedforward
-      public static final double kArmS = 0.12855;
-      public static final double kArmG = 0.14945;
-      public static final double kArmV = 0.050359;
-
-      public static final double  kArmGearRatio = 228.5;
-      public static final double  kLowGearRatio = 18.75;
-    
-  //Intake.java
-      //CANIDS
-      public static final int LeftIntakeCANID = 21;
-      public static final int RightIntakeCANID = 22;
 
 }
